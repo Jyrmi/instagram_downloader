3280b0e5c1655abfb9babf57bc3d4445933823df
Jyrmi <jeremyschoi@gmail.com>
2016-06-08 03:57:42 -0400

    serialize files, resolve exceptions, and update readme

--- README.md
+++ README.md
@@ -10,9 +10,6 @@ or
 
 python bin/run.py https://www.instagram.com/pythonpaige/
 
-
-
-
 You may optionally specify the directory to download to:
 
 
@@ -21,3 +18,27 @@ python bin/run.py --dest pics pythonpaige
 or
 
 python bin/run.py --dest ~/Desktop/instagram_images pythonpaige
+
+You may also optionally indicate whether to serialize the downloaded files. Without the option, file names will be their original web ID's, e.g.
+
+11142783_1577741432494889_787295718_n.jpg
+
+11189805_917626121591623_583183794_n.jpg
+
+11184547_1414354925550367_274565051_n.jpg
+
+etc.
+
+but running
+
+python bin/ run.py --serialize pythonpaige
+
+will result in file names being followed by their number, i.e.
+
+(1) 11142783_1577741432494889_787295718_n.jpg
+
+(2) 11189805_917626121591623_583183794_n.jpg
+
+(3) 11184547_1414354925550367_274565051_n.jpg
+
+where the earliest posted image is number (1).
--- src/run.py
+++ src/run.py
@@ -10,11 +10,9 @@ import itertools
 from urllib2 import urlopen
 from getopt import getopt
 from time import sleep
-from bs4 import BeautifulSoup
 from selenium.webdriver import Firefox # pip install selenium
-# from selenium.webdriver.common.keys import Keys
 
-# url and path related stuff
+
 insta_url = 'https://www.instagram.com/'
 
 # --- Pattern definitions ---
@@ -23,90 +21,70 @@ insta_url = 'https://www.instagram.com/'
 #   url.
 file_name = re.compile(r'[a-zA-Z0-9_-]+\.(jpg|mp4)')
 
-# - resize: websta displays a smaller version of all Instagram images, which
-#   I'm guessing Instagram created created for other purposes. There's
+# - resize: Instagram sometimes displays a smaller version of images, which
+#   I'm guessing Instagram created created for thumbnails. There's
 #   consistently some extra stuff included in the middle of the original image
 #   url to reference the smaller versions, like s640x640/sh0.08/ or the like.
 #   We can just sub that stuff out and end up with the url to the og image.
 resize = re.compile(r'[^/]+\d+x\d+/(sh\d+\.\d+/)?')
 
-def delete_item(driver, n_times):
+# - crop: same thing as with resize here; something like c86.0.550.550/ is
+#   added to the url to crop the image for thumbnails. Sub it out.
+crop = re.compile(r'c[\d\.]+/')
+
+
+def download_from_url(url, directory='./', serialize=False, number=0):
+    fname = file_name.search(url).group(0)
+
+    if serialize:
+        fname = '(' + str(number) + ') ' + fname
+
+    dl_path = directory + '/' + fname
+
+    response = urlopen(url)
+    with open(dl_path, 'wb') as out:
+        out.write(response.read())
+
+    # DEBUG
+    print dl_path
+
+
+def fetch_row_links(driver):
     """
-    Deletes the first media item that appears on the profile.
+    Gets the url of each image from the top row (three items) on the profile.
     """
-    # print 'deleting %d item(s)' % n_times
+    urls = []
+    for image in driver.find_elements_by_class_name('_icyx7')[:3]:
 
-    for _ in itertools.repeat(None, n_times):
-        script_find_item = "var aa=document.getElementsByClassName('_8mlbc _vbtk2 _t5r8b')[0];"
-        script_delete_item = "aa.parentNode.removeChild(aa)"
-        driver.execute_script(script_find_item + script_delete_item)
+        resized = resize.sub('', image.get_attribute('src'))
+        link = crop.sub('', resized)
 
-# def delete_row(driver, n_times):
-#     """
-#     Deletes the top row (three items) on the profile.
-#     """
-#     print 'deleting %d row(s)' % n_times
-#     delete_item(driver, n_times * 3)
+        urls.append(link)
 
-def delete_row(driver, n_times):
+    return urls
+
+
+def delete_row(driver):
     """
-    Deletes the top row (three items) on the profile.
+    Removes the top row of images from view (by deleting them from the DOM).
     """
-    print 'deleting %d row(s)' % n_times
+    find_row = "var aa=document.getElementsByClassName('_myci9')[0];"
+    remove_row = "aa.parentNode.removeChild(aa)"
+    try:
+        driver.execute_script(find_row + remove_row)
+    except:
+        print 'exception: no more images'
+        exit(0)
 
-    for _ in itertools.repeat(None, n_times):
-        script_find_row = "var aa=document.getElementsByClassName('_myci9')[0];"
-        script_delete_row = "aa.parentNode.removeChild(aa)"
-        driver.execute_script(script_find_row + script_delete_row)
-
-def download_and_delete_row(driver, n_times, output_directory):
-    """
-    Deletes the top row (three items) on the profile.
-    """
-    # print 'dl\'ing & deleting %d row(s)' % n_times
-
-    images = driver.find_elements_by_tag_name('img')
-    for i in xrange(n_times * 3):
-        link = resize.sub('', images[i].get_attribute('src'))
-        fname = file_name.search(link).group(0)
-        dl_path = output_directory + '/' + fname
-
-        response = urlopen(link)
-        with open(dl_path, 'wb') as out:
-            out.write(response.read())
-
-            # DEBUG
-            print dl_path
-
-
-    for _ in itertools.repeat(None, n_times):
-        script_find_row = "var aa=document.getElementsByClassName('_myci9')[0];"
-        script_delete_row = "aa.parentNode.removeChild(aa)"
-        driver.execute_script(script_find_row + script_delete_row)
 
 def count_items(driver):
     """
     Counts the number of media items that appears on the profile.
     """
     items = driver.find_elements_by_class_name('_22yr2')
-    count = len(items)
 
-    # print '%d photos/images on page currently' % count
+    return len(items)
 
-    return count
-
-def print_tags(driver):
-    """
-	Prints all the media item tags on the current page.
-	"""
-    media_links = []
-
-    media_tags = driver.find_elements_by_class_name('_icyx7')
-    for media_item in media_tags:
-        link = media_item.get_attribute('src')
-        media_links.append(link)
-    print media_links
-    print len(media_tags), 'items'
 
 def main():
     """
@@ -117,9 +95,11 @@ def main():
     # are downloading. Must be supplied.
     output_directory = '' # Will be initialized with the optional argument or a
     # default later.
-    
+    update_mode = False
+    serialize = False
+
     # --- Argument Parsing ---
-    opts, args = getopt(sys.argv[1:], '', ['dest='])
+    opts, args = getopt(sys.argv[1:], '', ['dest=', 'update', 'serialize'])
 
     # Expecting only one argument that isn't an option, which is the Instagram
     # username or profile url. We only want the username for our purposes so strip
@@ -140,6 +120,10 @@ def main():
             output_directory = option_argument
             if not os.path.exists(output_directory):
                 os.makedirs(output_directory)
+        elif option == '--update':
+            update_mode = True
+        elif option == '--serialize':
+            serialize = True
 
     # If the output directory doesn't exist, create it.
     if not output_directory:
@@ -147,82 +131,33 @@ def main():
         if not os.path.exists(profile_username):
             os.makedirs(profile_username)
 
-    print profile_username, output_directory
-
-    # max_queue = 30 # maximum number of items in queue before emptying
-    loaded = 0
-    processed = 0
-
+    # Start the browser
     driver = Firefox()
     driver.get(insta_url + profile_username)
 
     # Find the number of posts on this Instagram profile
-    profile_soup = BeautifulSoup(driver.page_source, 'lxml')
-    post_count_tag = profile_soup.find('span', {'class': '_e8fkl'})
-    post_count = int(post_count_tag.text.replace(',', ''))
-
-    # Zoom right out
-    # html = driver.find_element_by_tag_name("html")
-    # html.send_keys(Keys.CONTROL, Keys.SUBTRACT)
-    # html.send_keys(Keys.CONTROL, Keys.SUBTRACT)
-    # html.send_keys(Keys.CONTROL, Keys.SUBTRACT)
-    # html.send_keys(Keys.CONTROL, Keys.SUBTRACT)
-    # html.send_keys(Keys.CONTROL, Keys.SUBTRACT)
-    # html.send_keys(Keys.CONTROL, Keys.SUBTRACT)
-    # html.send_keys(Keys.CONTROL, Keys.SUBTRACT)
+    post_count_tag = driver.find_element_by_class_name('_e8fkl')
+    post_count = int(post_count_tag.text)
 
     # Click the 'Load More' element
     driver.find_elements_by_class_name('_oidfu')[0].click()
-    driver.execute_script("window.scrollTo(0, -1000000);")
-    # loaded = 24
 
     # Load all the posts into the browser
-    while loaded < post_count:
-
-        # print 'processed %d' % processed
-
-        # driver.execute_script("window.scrollTo(0, 1000000);")
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        sleep(.1)
-        driver.execute_script("window.scrollTo(0, 0);")
-        sleep(1)
-
-        while count_items(driver) > 24 or post_count - processed < 24:
-        # while count_items(driver) >= 24:
-            download_and_delete_row(driver, 4, output_directory)
-            # try:
-            #     # delete_row(driver, 4)
-            #     download_and_delete_row(driver, 4)
-            # except:
-            #     break
-
-            processed += 12
-
-        # print 'loaded', loaded, '\tprocessed', processed
-
-        # if loaded - processed >= max_queue or loaded >= post_count:
-
-        #     # print_tags(driver)
-        #     print 'EMPTYING QUEUE'
-
-        #     # Repeats <loaded> times
-        #     for _ in itertools.repeat(None, (loaded-processed)/3):
-        #         delete_row(driver)
-        #         processed += 3
-
-
+    processed = 0
+    while processed < post_count:
         # Load more content by scrolling to the bottom of the page
-        # last_position = driver.execute_script('return window.scrollY;')
+        driver.execute_script("window.scrollTo(0, 0);")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        # driver.execute_script("window.scrollTo(0, 1000000);")
-        # sleep(1.5)
-
-        # new_position = driver.execute_script('return window.scrollY;')
-        # print last_position, '>', new_position
-
-        # If the browser successfully loaded more content, count it
-        # if last_position != new_position:
-        #     loaded += 12
+        # Download 4 rows of items (4 rows are loaded upon each scroll) and
+        # remove them from view
+        for _ in itertools.repeat(None, 4):
+            urls = fetch_row_links(driver)
+            for url in urls:
+                download_from_url(url, output_directory,
+                                  True, post_count-processed)
+                processed += 1
+            delete_row(driver)
 
     driver.close()
 
